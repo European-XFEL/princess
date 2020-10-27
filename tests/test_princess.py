e02ccced881c96bd5c8884f3d8c8a2052507a1ba
Thomas Kluyver <thomas@kluyver.me.uk>
2020-10-27 17:00:45 +0000

    Tests for saving resulting notebook

--- tests/test_princess.py
+++ tests/test_princess.py
@@ -1,4 +1,7 @@
+import nbformat
 from pathlib import Path
+import shutil
+
 from princess import main
 
 this_dir = Path(__file__).parent
@@ -11,3 +14,25 @@ def test_output(capsys):
     assert 'Print to stderr\n' in captured.err
     assert 'Display text' in captured.out
     assert 'ZeroDivisionError' in captured.out
+
+
+def test_save(tmp_path):
+    nb_orig = nbformat.read(this_dir / 'Sample.ipynb', as_version=4)
+    assert nb_orig.cells[0].outputs == []
+
+    shutil.copy2(this_dir / 'Sample.ipynb', tmp_path)
+    main([str(tmp_path / 'Sample.ipynb'), '--save'])
+
+    nb_saved = nbformat.read(tmp_path / 'Sample.ipynb', as_version=4)
+    assert nb_saved.cells[0].outputs[0].text == 'Print to stdout\n'
+
+
+def test_save_as(tmp_path):
+    output_path = tmp_path / 'Executed.ipynb'
+    main([str(this_dir / 'Sample.ipynb'), '--save-as', str(output_path)])
+
+    nb_orig = nbformat.read(this_dir / 'Sample.ipynb', as_version=4)
+    assert nb_orig.cells[0].outputs == []
+
+    nb_saved = nbformat.read(output_path, as_version=4)
+    assert nb_saved.cells[0].outputs[0].text == 'Print to stdout\n'
